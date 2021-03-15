b19ed00c64c376da88b85353c7aca025ad2e1c79
Denisse Rivas <iqrivas@gmail.com>
2021-03-14 23:44:22 -0600

    adding CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 const path = require('path');
 const petitionAPIRouter = require('./routes/api/petitions');
@@ -7,6 +8,23 @@ const petitionAPIRouter = require('./routes/api/petitions');
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+//CORS
+const allowedOrigins = ['http://localhost:3000',
+                      'https://iqrivas.github.io/altardepterio'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(express.json());
 app.use("/api/petitions", petitionAPIRouter)
